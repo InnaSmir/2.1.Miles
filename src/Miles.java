733604bafdfb4b450695bb295e5f364d9e5f04cb
Inna Smirnova <moipr@yandex.ru>
2020-06-10 19:20:00 +0300

    Added new files

---
+++ src/Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[]args) {
+        int priceTicket = 36_750;
+        int miles = priceTicket / 20;
+        System.out.println(miles);
+    }
+}
+
